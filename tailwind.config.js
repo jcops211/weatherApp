013d5d0adfaf1aeee4ad3cceb92e7eb5ec134d09
ja8o@5acUfiiF <jack.dudley340@gmail.com>
2023-05-11 19:05:50 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      cyan: colors.cyan
+
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
