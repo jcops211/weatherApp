e8da2c268655b66a009975a0c168e04cfb54e672
ja8o@5acUfiiF <jack.dudley340@gmail.com>
2023-05-12 17:56:07 -0400

    added ajax apiGet

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
     let apiKey = 'fcc0735fb36e11df3f7e3670083ed6c1';
-    let url = 'https://api.weatherstack.com/';
+    let url = 'http://api.weatherstack.com/current';
+    $('#test').hide().fadeIn(5000).slideDown();
+    $('#main-btn').on("click", function () {
+        let input = ($("#input-text").val());
+        $.get({
+            url: url,
+            data: {"access_key": apiKey,"query":input, "units":"f"},
+            success: function(response) {
+                console.log("success!", response);
+            },
+            error: function(xhr) {
+                console.log("Failed!");
+            }
+        }
+
+        )
+    })
 })
