8c28f09619921749d5cddbede76b6c3be52472ad
ja8o@5acUfiiF <jack.dudley340@gmail.com>
2023-05-13 16:37:33 -0400

    get api request to work

--- js/main.js
+++ js/main.js
@@ -5,15 +5,24 @@ $(document).ready(function(){
     $('#main-btn').on("click", function () {
         let input = ($("#input-text").val());
         $.get({
-            url: url,
-            data: {"access_key": apiKey,"query":input, "units":"f"},
-            success: function(response) {
-                console.log("success!", response);
-            },
-            error: function(xhr) {
-                console.log("Failed!");
+                url: url,
+                data: {"access_key": apiKey,"query":input, "units":"f"},
+                success: function(response) {
+                    console.log("success!", response);
+                    let temp = response.current.temperature;
+                    let humidity = response.current.humidity;
+                    let precip = response.current.precip;
+                    let uvIndex = response.current.uv_index;
+                    let currTime = response.location.localtime;
+                    let description = response.current.weather_descriptions[0];
+                    console.log(description);
+                    window.location.href = "weatherShow.html";
+
+                },
+                error: function(xhr) {
+                    console.log("Failed!");
+                }
             }
-        }
 
         )
     })
--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     './components/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sunny': "url('../img/sunnyReal.jpg')",
+        'partlyCloudy': "url('../img/partlyCloudy.jpg')",
+        'rain': "url('../img/rain.png')",
+        'overcast': "url('../img/overcast.jpg')",
+        'misty': "url('../img/misty.jpg')",
+        'snow': "url('../img/snow.jpg')",
+      }
+    },
   },
   plugins: [],
 }
